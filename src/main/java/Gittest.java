80d67a3b4cb1127ff0b1638884dcec0e6112082a
wubi <wubi@qq.com>
2022-05-27 16:29:58 +0800

    commit1

--- src/main/java/Gittest.java
+++ src/main/java/Gittest.java
@@ -2,5 +2,8 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("idea链接github");
         System.out.println("commit1");
+        System.out.println("hot-fix 输出1");
+        System.out.println("github hot-fix 输出1");
+        
     }
 }
